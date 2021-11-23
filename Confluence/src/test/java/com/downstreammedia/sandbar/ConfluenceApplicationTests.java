eb9bb2d06de838647480c5cec496c1491b5bad40
Diego Hoyos <hoyosdiegoa@gmail.com>
2021-11-23 12:31:24 -0700

    Created and configured Confluence Spring Boot project. Added JPASanBar project as dependency and configured connection to DB in application.properties

---
+++ Confluence/src/test/java/com/downstreammedia/sandbar/ConfluenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.downstreammedia.sandbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfluenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
