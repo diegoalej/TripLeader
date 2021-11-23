531109152950acfb248341b5de935fd74e2fcc34
Diego Hoyos <hoyosdiegoa@gmail.com>
2021-11-23 14:16:07 -0700

    Fixed error on category entity

--- JPASandBar/src/main/java/com/downstreammedia/sandbar/entities/Category.java
+++ JPASandBar/src/main/java/com/downstreammedia/sandbar/entities/Category.java
@@ -2,17 +2,13 @@ package com.downstreammedia.sandbar.entities;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 @Entity
 public class Category {
