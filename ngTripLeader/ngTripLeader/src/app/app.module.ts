9ab866a282bdfe19ec4eb224535b178775eab536
Diego Hoyos <hoyosdiegoa@gmail.com>
2021-12-04 15:00:59 -0700

    cleaned up app.module.ts

--- ngTripLeader/ngTripLeader/src/app/app.module.ts
+++ ngTripLeader/ngTripLeader/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './components/nav/nav.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent,
-    FooterComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
