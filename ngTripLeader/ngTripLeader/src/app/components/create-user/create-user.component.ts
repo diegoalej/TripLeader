e9b2b1df1076df4b3d9e18aed46ed7947c682674
Diego Hoyos <hoyosdiegoa@gmail.com>
2021-12-13 15:39:43 -0700

    Updated create user component to create accounts

--- ngTripLeader/ngTripLeader/src/app/components/create-user/create-user.component.ts
+++ ngTripLeader/ngTripLeader/src/app/components/create-user/create-user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-create-user',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateUserComponent implements OnInit {
 
-  constructor() { }
+  // newUser: User = new User(1, " ", " "," ",true, "user", " "," "," ");
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  register(form: NgForm) {
+    const user: User = form.value;
+    this.auth.register(user).subscribe(
+      data => {
+        console.log('RegisterComponent.register(): user registered.');
+        this.auth.login(user.username, user.password).subscribe(
+          next => {
+            console.log('RegisterComponent.register(): user logged in, routing to /home.');
+            this.router.navigateByUrl('/trips');
+          },
+          error => {
+            console.error('RegisterComponent.register(): error logging in.');
+          }
+        );
+      },
+      err => {
+        console.error('RegisterComponent.register(): error registering.');
+        console.error(err);
+      }
+    );
   }
 
 }
